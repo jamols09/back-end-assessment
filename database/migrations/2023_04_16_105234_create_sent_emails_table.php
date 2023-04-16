07062a269d47fb3132f3117f82f290e9593f6d55
jamols09 <jamols09@github.com>
2023-04-16 19:33:44 +0800

    add migration

---
+++ database/migrations/2023_04_16_105234_create_sent_emails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sent_emails', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('body');
+            $table->string('title');
+            $table->dateTime('send_datetime');
+            $table->foreignId('email_template_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sent_emails');
+    }
+};
